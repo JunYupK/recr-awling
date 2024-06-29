ff62307c0aa563668f94a794cf5e1e93a863396e
yongbul <top1506@naver.com>
2024-06-29 21:18:59 +0900

    init

---
+++ src/main/java/com/recruit/recrawling/RecrAwlingApplication.java
@@ -0,0 +1,13 @@
+package com.recruit.recrawling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecrAwlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecrAwlingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recruit/recrawling/RecrAwlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recruit.recrawling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecrAwlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
