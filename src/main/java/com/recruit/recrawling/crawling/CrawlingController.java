71f2c2edbe70538f859cd13591a5479ea47a1825
yongbul <top1506@naver.com>
2024-06-29 21:36:01 +0900

    feat : crawling controller 구조 완성

---
+++ src/main/java/com/recruit/recrawling/crawling/CrawlingController.java
@@ -0,0 +1,10 @@
+package com.recruit.recrawling.crawling;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/crwaling")
+public class CrawlingController {
+
+}
