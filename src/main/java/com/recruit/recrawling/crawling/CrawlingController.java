e395604ee577760c7b0fa0cadc21763001e2d6f6
yongbul <top1506@naver.com>
2024-07-01 22:37:54 +0900

    feat : 크롬 드라이버 연결 및 네이버 페이지 가져오기 성공

--- src/main/java/com/recruit/recrawling/crawling/CrawlingController.java
+++ src/main/java/com/recruit/recrawling/crawling/CrawlingController.java
@@ -1,10 +1,19 @@
 package com.recruit.recrawling.crawling;
 
+import com.recruit.recrawling.crawling.service.RecruitCrawlingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/crwaling")
 public class CrawlingController {
-
+    @Autowired
+    private RecruitCrawlingService recruitCrawlingService;
+    @GetMapping("check")
+    public String check(){
+        return recruitCrawlingService.initCrawlingService();
+    }
 }
---
+++ src/main/java/com/recruit/recrawling/crawling/service/RecruitCrawlingService.java
@@ -0,0 +1,44 @@
+package com.recruit.recrawling.crawling.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+@Service
+@Slf4j
+public class RecruitCrawlingService {
+    private static String WEB_DRIVER_ID = "webdriver.chrome.driver";
+    private static String WEB_DRIVER_PATH = "C:\\Users\\top15\\Downloads\\chromedriver-win64\\chromedriver.exe";
+    public String initCrawlingService(){
+        String source = "";
+        System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        options.addArguments("--disable-popup-blocking");
+        WebDriver webDriver = new ChromeDriver(options);
+
+        log.info("웹 드라이버 세팅 완료");
+        try{
+            log.info("홈페이지 접속");
+            webDriver.get("https://www.naver.com");
+            log.info("2초 대기");
+            Thread.sleep(2000);
+            source = webDriver.getPageSource();
+        }catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            webDriver.close();
+        }
+        System.out.println(source);
+        return source;
+    }
+}
